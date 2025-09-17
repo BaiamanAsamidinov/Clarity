5e63d06b21d734d0c389bf1ba36604e3a4707a36
Baiaman <baiaman.asamidinov@gmail.com>
2025-09-17 05:06:40 +0200

    Initial commit

---
+++ src/main/java/com/now/Clarity/ClarityApplication.java
@@ -0,0 +1,13 @@
+package com.now.Clarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClarityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClarityApplication.class, args);
+	}
+
+}
