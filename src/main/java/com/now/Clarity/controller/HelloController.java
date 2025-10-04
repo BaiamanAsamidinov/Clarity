af6f2d75030775a44e236d710c4a19336bf1334f
Baiaman <baiaman.asamidinov@gmail.com>
2025-10-04 12:48:36 +0200

    Structure to logically organize different components of the application

--- src/main/java/com/now/Clarity/controller/HelloController.java
+++ src/main/java/com/now/Clarity/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.now.Clarity;
+package com.now.Clarity.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
